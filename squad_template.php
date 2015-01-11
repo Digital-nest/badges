03d32ceb1918515599ea37aa843a3944e191fc9f
Vivian Tong <VivianTong@eduroam-240-181.ucsc.edu>
2015-01-11 13:11:56 -0800

    add fields from database for jobs

--- squad_template.php
+++ squad_template.php
@@ -31,34 +31,52 @@ $category_jobs = dn_get_category_jobs($id);
      <div class="row">
        <div class="col-sm-6">
        <h4>Business:&nbsp;</h4>   
+       <?php echo $job['business_name']?>
     </div>
        <div class="col-sm-6">
        <h4>Name:&nbsp;</h4>
+       <?php echo $job['contact_name'] ?>
         </div>
     </div>
    
     <div class="row">
        <div class="col-sm-6">
        <h4>Email:&nbsp;</h4>
-       <?php echo $job->contact_email?>
+       <?php echo $job['contact_email']?>
     </div>   
     <div class="col-sm-6">
        <h4>Phone Number:&nbsp;</h4> 
-       <?php echo $job->contact_phone?>  
+       <?php echo $job['contact_phone']?>  
     </div>
     </div>
     
     <div class="row">
        <div class="col-sm-6">
        <h4>Duration of Employment:&nbsp;</h4>   
+       <?php echo $job['duration']?>
     </div>
        <div class="col-sm-6">
        <h4>Paid:&nbsp;</h4>
+       <?php if ($job['paid']){
+                 echo 'Yes';
+        	} else {
+        	    echo 'No';
+        	}
+       ?>
         </div>
     </div>
         
     <h4>Description:&nbsp;</h4>   
+    <?php echo $job['description']?>
     <h4>Need:&nbsp;</h4>
+    <?php echo $job['need']?>  
+    
+    <h4>Skills Needed:&nbsp</h4>
+    <ul>
+    <?php foreach (dn_get_job_skills($id) as $skill): ?>
+       <li><?php echo $skill['name']; ?>
+    <?php endforeach ?>
+    </ul> 
     <?php endforeach; ?>
 </div>
 </section>
